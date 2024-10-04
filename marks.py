2bf70e9a2d6b48b21404aaa0cd4f4f44f9c50129
Saran-sk52 <saransk1249@gmail.com>
2024-10-04 11:52:04 +0530

    Delete marks.py

--- marks.py
+++
@@ -1,8 +0,0 @@
-sub1 = int(input("Enter the mark in this subject"))
-sub2 = int(input("Enter the mark in this subject"))
-sub3 = int(input("Enter the mark in this subject"))
-sub4 = int(input("Enter the mark in this subject"))
-sub5 = int(input("Enter the mark in this subject"))
-total= sub1 + sub2 + sub3 + sub4 + sub5
-print("Total marks = ",total)
-print("% of marks =", total / 500*100)
