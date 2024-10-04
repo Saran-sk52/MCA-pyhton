7e116d87c019dc3f61451e72d38649ddfba3db55
Saran-sk52 <saransk1249@gmail.com>
2024-10-04 12:02:04 +0530

    Add files via upload

---
+++ September/ArthematicOp.py
@@ -0,0 +1,7 @@
+n1 = int(input("Enter first number"))
+n2 = int(input("Enter second number"))
+         
+print("Addition -",n1 + n2)
+print("Subtraction -",n1 - n2)
+print("Devision -",n1 / n2)
+print("Multiplication -",n1 * n2)
