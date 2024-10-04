a5b5f7eab2eb0952b3044fb9a13c3b77b16a13c4
Saran-sk52 <saransk1249@gmail.com>
2024-10-04 12:03:32 +0530

    Delete September directory

--- September/AreaofRECTANGLE.py
+++
@@ -1,3 +0,0 @@
-l = int(input("Enter the length of the rectangle"))
-b = int(input("Enter the breadth of the rectangle"))
-print("Area of the rectangle =",l*b)
--- September/AreaofTRIANGLE.py
+++
@@ -1,3 +0,0 @@
-h = int(input("Enter the height of the triangle"))
-b = int(input("Enter the base of the triangle"))
-print("Area of the triangle =",1/2*b*h)
