95513ca3dcc49b3dde0726399144fc7b26a03a60
Saran-sk52 <saransk1249@gmail.com>
2024-10-25 16:08:59 +0530

    Add files via upload

---
+++ October/25-10-2024/fibonacci.py
@@ -0,0 +1,9 @@
+r = int(input("Enter the range:"))
+lst=[]
+for i in range(r):
+    if i==0 or i==1:
+        lst.append(i)
+    else:
+        number=lst[i-1]+lst[i-2]
+        lst.append(number)
+print(lst)
