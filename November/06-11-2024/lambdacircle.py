c0e503de8e397a230a7611da04c09128cb714474
Saran-sk52 <saransk1249@gmail.com>
2024-11-06 14:55:17 +0530

    Add files via upload

---
+++ November/06-11-2024/lambdacircle.py
@@ -0,0 +1,3 @@
+c=lambda a:3.145*a*a
+n = int(input("Enter the radius of the circle: "))
+print("Area of the circle is ",c(n))
---
+++ November/06-11-2024/lambdacube.py
@@ -0,0 +1,3 @@
+c=lambda a:a*a*a
+n = int(input("ENter a number: "))
+print("cube of the number is ",c(n))
